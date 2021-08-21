7b236c24e87fd3fd872e1c642a74303af85bcd79
Brainz876 <raheemavatar@gmail.com>
2021-08-21 03:41:04 -0500

    Reconfigure Server for Heroku

--- server/server.js
+++ server/server.js
@@ -6,8 +6,6 @@ const path = require("path");
 // parse env variables
 require('dotenv').config();
 
-require("./helpers/db/mongodb.js")();
-
 // Configuring port
 const port = process.env.PORT || 3000;
 
@@ -18,7 +16,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
 
-app.set('view engine', 'html');
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(__dirname + "/dist/"));
@@ -26,10 +23,6 @@ if (process.env.NODE_ENV === "production") {
 		res.sendFile(path.resolve(__dirname + "dist" + "index.html"));
 	});
 }
-
-// Defining route middleware
-app.use('/api', require('./routes/api'));
-
 // Listening to port
 app.listen(port);
 console.log(`Listening On http://localhost:${port}/api`);

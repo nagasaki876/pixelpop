adff273f4cf92b5445a09faee075d93fc8731972
Brainz876 <raheemavatar@gmail.com>
2021-08-21 03:39:14 -0500

    Minor Changes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Importing required modules
 const cors = require('cors');
 const express = require('express');
+const path = require("path");
 
 // parse env variables
 require('dotenv').config();
@@ -8,18 +9,23 @@ require('dotenv').config();
 require("./helpers/db/mongodb.js")();
 
 // Configuring port
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 // Configure middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 
 app.set('view engine', 'html');
 
-// Static folder
-app.use(express.static(__dirname + '/views/'));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(__dirname + "/dist/"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname + "dist" + "index.html"));
+	});
+}
 
 // Defining route middleware
 app.use('/api', require('./routes/api'));

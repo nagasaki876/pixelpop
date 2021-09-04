52a7da740bf3fb5b2fbeaebded33ea693c5be618
Brainz876 <raheemavatar@gmail.com>
2021-09-04 12:26:06 -0500

    Added Footer and Meta Data, Updated Text

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -2,58 +2,34 @@
   <v-app>
     <app-bar />
 
-    <v-row class="px-5">
-      <v-col>
-        <p class="text-h3 font-weight-light">
-          Design-led strategies that <br />
-          impact people’s life.
-        </p>
-      </v-col>
-    </v-row>
-
-    <!-- Images -->
-    <v-row>
-      <v-col>
-        <v-img></v-img>
-      </v-col>
-    </v-row>
-
-    <v-col class="pa-0">
-      <v-col align="center" class="pixelgreen pt-16 px-16">
-        <v-card flat width="500" color="pixelgreen">
-          <p class="text-left text-h4 font-weight-light">We’re global</p>
-          <p class="text-left text-h6">
-            Solid communication, iteration and prototyping allows us to work
-            alongside clients from all around the world. We have a big office in
-            Porto, Portugal that allows us to host project workshops,
-            brainstorming sessions and work side by side with you and your team.
-            Just like being neighbours, you can drop by for some eggs, anytime.
-            We also travel regularly to do the exact same thing with the added
-            bonus of getting to know new places!
+    <v-row justify="center">
+      <v-col cols="6">
+        <v-container>
+          <p class="text-center text-h3 font-weight-light">
+            Design-led strategies that <br />
+            impact people’s life.
           </p>
-        </v-card>
+        </v-container>
       </v-col>
-      <v-img :src="require('@/assets/world.png')">
-        <v-row class="pa-5">
-          <v-col
-            ><p class="text-h4 font-weight-light">
-              Life here, in the heart <br />
-              of Kingston
-            </p></v-col
-          >
+    </v-row>
 
-          <v-col>
-            <p>
-              **We spend a great deal of our days at work, so why not make it
-              interesting? With a friendly and laid back environment you get all
-              the perks to ensure a creative and productive day.
-            </p>
-          </v-col>
-        </v-row>
-      </v-img>
-    </v-col>
-
-    <v-col align="center">
+    <!-- About -->
+    <v-row justify="center">
+      <v-col cols="11">
+        <v-container>
+          <p class="text-h6 font-weight-light">
+            I work as a User Interface & User Experience designer creating
+            thoughtful experiences with the combination of design, business and
+            marketing. I’m passionate about building & designing thoughtful
+            experiences to make sure your customers and users are satisfied when
+            they’re using your products and services online. I also love
+            documenting my journey and sharing it with the community to help
+            others succeed and grow.
+          </p>
+        </v-container>
+      </v-col>
+    </v-row>
+    <v-col class="mt-16" align="center">
       <p class="text-h4 font-weight-light">From inception to launch.</p>
       <p>Get to know our process a little better.</p>
       <v-btn text
@@ -62,16 +38,22 @@
         ></v-btn
       >
     </v-col>
+    <pixel-footer />
   </v-app>
 </template>
 
 <script>
 import AppBar from "../components/AppBar.vue";
+import PixelFooter from "../components/PixelFooter.vue";
 export default {
-  components: { AppBar },
+  components: { AppBar, PixelFooter },
   data() {
     return {};
   },
+  metaInfo: {
+    title: "About",
+    titleTemplate: "%s | PixelPop"
+  }
 };
 </script>
 

866d67668b9055847607b3b6d9bd2b26ed96e1a0
Brainz876 <raheemavatar@gmail.com>
2021-09-04 12:39:00 -0500

    Added Meta Data, Raleway Font

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,19 +6,33 @@
 
 <script>
 export default {
-  data: () => ({
-    //
-  }),
+  name: "App",
+  data() {
+    return {};
+  },
+  metaInfo: {
+    title: "App",
+    titleTemplate: "%s | PixelPop",
+    htmlAttrs: {
+      lang: "en-US"
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "description", content: "" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" }
+    ]
+  }
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;400;500;600;700;900&display=swap');html,
+@import url("https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;400;500;600;700;900&display=swap");
+html,
 body {
   font-family: "Raleway", sans-serif;
 }
 
 .v-application {
-   font-family: "Raleway", sans-serif !important;
+  font-family: "Raleway", sans-serif !important;
 }
 </style>

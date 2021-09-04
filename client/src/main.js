4301942924abe367bc0839be36f595ae33f906cf
Brainz876 <raheemavatar@gmail.com>
2021-09-04 12:39:20 -0500

    Added Vue-Meta

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import vuetify from './plugins/vuetify'
+import vuetify from "./plugins/vuetify";
+import VueMeta from "vue-meta";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueMeta);
 
 new Vue({
   router,

48f2295098611b0150219a5714f8bd4ad9df997a
Brainz876 <raheemavatar@gmail.com>
2021-09-04 12:40:16 -0500

    Refactor

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,23 +1,22 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
-import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
-
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+import "material-design-icons-iconfont/dist/material-design-icons.css"; // Ensure you are using css-loader
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    theme: {
-        themes: {
-          light: {
-            pixelred: "#F03735", // #E53935
-            pixelblue: "#1789FA", // #FFCDD2
-            pixelyellow: "#F4D12F",
-            pixelgreen: "#37B431", // #3F51B5
-            pixelorange: "#F7B22D"
-          }
-        }
-      },
-      icons: {
-        iconfont: 'md'
+  theme: {
+    themes: {
+      light: {
+        pixelred: "#F03735", // #E53935
+        pixelblue: "#1789FA", // #FFCDD2
+        pixelyellow: "#F4D12F",
+        pixelgreen: "#37B431", // #3F51B5
+        pixelorange: "#F7B22D"
       }
+    }
+  },
+  icons: {
+    iconfont: "md"
+  }
 });
